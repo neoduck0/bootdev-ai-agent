60b107f0f8b2d746759215c54dfc7c7351a23b58
neoduck0 <219318428+neoduck0@users.noreply.github.com>
2026-09-22 15:11:56 +0800

    3-1

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 from openai import OpenAI
 from openai.types.chat import ChatCompletionMessageParam
 
+from prompts import system_prompt
+
 
 def main():
     load_dotenv()
@@ -22,12 +24,12 @@ def main():
     )
 
     messages: list[ChatCompletionMessageParam] = [
-        {"role": "user", "content": args.user_prompt}
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": args.user_prompt},
     ]
 
     response = client.chat.completions.create(
-        model="openrouter/free",
-        messages=messages,
+        model="openrouter/free", messages=messages, temperature=0
     )
 
     if response.usage == None:
