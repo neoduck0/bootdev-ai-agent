8ee7aac2faa0ffab16a87acbffc1d68d337db359
neoduck0 <219318428+neoduck0@users.noreply.github.com>
2026-09-23 12:45:51 +0800

    4-1

--- main.py
+++ main.py
@@ -29,31 +29,46 @@ def main():
         {"role": "system", "content": system_prompt},
         {"role": "user", "content": args.user_prompt},
     ]
-    response = client.chat.completions.create(
-        model="openrouter/free",
-        temperature=0,
-        messages=messages,
-        tools=available_functions,
-    )
 
-    if response.usage == None:
-        raise RuntimeError("response.usage is None, likely failed API request")
+    for _ in range(20):
+        response = client.chat.completions.create(
+            model="openrouter/free",
+            temperature=0,
+            messages=messages,
+            tools=available_functions,
+        )
 
-    if args.verbose:
-        print(f"User prompt: {response.usage.prompt_tokens}")
-        print(f"Prompt tokens: {response.usage.prompt_tokens}")
-        print(f"Response tokens: {response.usage.completion_tokens}")
+        if response.usage == None:
+            raise RuntimeError("response.usage is None, likely failed API request")
 
-    tool_calls = response.choices[0].message.tool_calls
+        if args.verbose:
+            print(f"User prompt: {response.usage.prompt_tokens}")
+            print(f"Prompt tokens: {response.usage.prompt_tokens}")
+            print(f"Response tokens: {response.usage.completion_tokens}")
 
-    if tool_calls != None:
-        for tool_call in tool_calls:
-            arguments: dict[str, str] = json.loads(tool_call.function.arguments or "{}")
-            result = call_function(tool_call, args.verbose)
-            if args.verbose:
-                print(f"-> {result['content']}")
+        message = response.choices[0].message
+        messages.append(message)
 
-    print(response.choices[0].message.content)
+        tool_calls = message.tool_calls
+        if tool_calls != None:
+            for tool_call in tool_calls:
+                arguments: dict[str, str] = json.loads(
+                    tool_call.function.arguments or "{}"
+                )
+                print(f"- Calling function: {tool_call.function.name}")
+                result = call_function(tool_call, args.verbose)
+                messages.append(result)
+                if args.verbose:
+                    print(f"-> {result['content']}")
+
+        if message.content != None:
+            print(message.content)
+
+        if tool_calls == None:
+            exit(0)
+
+    print("Error: something went wrong")
+    exit(1)
 
 
 if __name__ == "__main__":
